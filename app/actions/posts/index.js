6bf7e75d3da8a0c4677e9c3235ae9f665ab0ee27
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-11 20:46:25 -0500

    Use candycane-jsonapi-mapper

--- app/actions/posts/index.js
+++ app/actions/posts/index.js
@@ -1,30 +1,19 @@
 import Action from 'candycane/dist/http/action';
-// import map from 'candycane-jsonapi-mapper/decorator';
+import map from 'candycane-jsonapi-mapper/dist/decorator';
 
-// @map('post')
+@map('post')
 export default class PostsIndex extends Action {
   /**
    * Simplified promise aware hook for finding data
    * @return {any} POJO or Promise of data
    */
   data() {
-    let val;
-    try {
-      return this.app.make(`store`).model(`post`).fetchAll({
-          withRelated: ['comments'],
-        }).then((data) => {
-          return data;
-        }, (err) => {
-          console.log(err);
-        });
-    } catch(err) {
-      console.log(err);
-
-      return 'foobar';
-    }
-
-    return val;
-
-
+    return this.app.make(`store`).model(`post`).fetchAll({
+        withRelated: ['comments'],
+      }).then((data) => {
+        return data;
+      }, (err) => {
+        console.log(err);
+      });
   }
 }
